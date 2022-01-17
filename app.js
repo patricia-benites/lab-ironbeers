965348bd323b7a9564a738ef842da63d29a17b0a
Patricia <patricia.benites.cava@gmail.com>
2022-01-17 21:14:58 +0100

    add footer, link and script to layout

--- app.js
+++ app.js
@@ -14,12 +14,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-app.set("view engine", "hbs");
+// ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  // .then(beersFromApi => res.render('beers', {beers: beersFromApi}))
+  .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+  .catch(error => console.log(error));
+  });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
